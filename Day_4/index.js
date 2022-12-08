9be29d7d8190a9efd601dee582d27b3dc7fb684a
liyasandeep <liyasandeep1@gmail.com>
2022-12-08 23:52:22 +0000

    task2 complete

--- Day_4/index.js
+++ Day_4/index.js
@@ -7,6 +7,7 @@ fs.readFile(__dirname + "/day4_Input.txt", "utf-8")
   })
   .then((eachPairArr) => {
     task1(eachPairArr);
+    task2(eachPairArr);
   });
 
 function task1(eachPairArr) {
@@ -28,5 +29,38 @@ function task1(eachPairArr) {
     )
       count++;
   });
-  console.log(count);
+  console.log(
+    "Task1: The number of assignment pairs where one range fully contains the other ",
+    count
+  );
+}
+
+function task2(eachPairArr) {
+  let count = 0;
+  let pairArr = [];
+  eachPairArr.map((element) => {
+    pairArr = element.split(",").map((i) => {
+      return i.split("-");
+    });
+
+    pairArr = pairArr.map((el) => {
+      return [parseInt(el[0]), parseInt(el[1])];
+    });
+    /* case1 -------
+                 --------
+
+       case2  ----------
+                 -----
+        
+        case3     --------
+                -------
+
+        case4    ----
+               --------
+    */
+    if (pairArr[0][1] >= pairArr[1][0] && pairArr[0][0] <= pairArr[1][1]) {
+      count++;
+    }
+  });
+  console.log("Task 2:The number of overlapping assignment pairs ", count);
 }
