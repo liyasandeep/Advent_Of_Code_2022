89d0417a07d99db03045ec5ce720ec96cd71ca69
liyasandeep <liyasandeep1@gmail.com>
2022-12-08 19:25:46 +0000

    task 2 solution

--- Day_3/index.js
+++ Day_3/index.js
@@ -1,16 +1,24 @@
 const fs = require("fs/promises");
-//   function task1() {}
 
-//   function task2() {}
+fs.readFile(__dirname + "/day3_Input.txt", "utf-8")
+  .then((contents) => {
+    const eachRucksackCompartments = contents.split("\n");
+    return eachRucksackCompartments;
+  })
+  .then((eachRucksackCompartments) => {
+    task1(eachRucksackCompartments);
+    task2(eachRucksackCompartments);
+  });
 
-fs.readFile(__dirname + "/day3_Input.txt", "utf-8").then((contents) => {
-  const eachRucksackCompartments = contents.split("\n");
-  const priorityArr = [];
+let priorityArr = [];
+
+// task 1
+function task1(eachRucksackCompartments) {
   eachRucksackCompartments.map((element) => {
     const newArray = [];
     newArray.push(element.substring(0, element.length / 2));
     newArray.push(element.substring(element.length / 2));
-    // console.log(newArray);
+
     const compartment1Set = new Set(newArray[0]); // to get unique elements
 
     const commonItem = [...newArray[1]].filter((i) => compartment1Set.has(i));
@@ -22,8 +30,51 @@ fs.readFile(__dirname + "/day3_Input.txt", "utf-8").then((contents) => {
       priorityArr.push(uniqueCommonItem[0].charCodeAt() - 38);
     }
   });
-  console.log(priorityArr);
-  console.log(priorityArr.reduce((a, c) => a + c, 0));
-  // task1();
-  // task2();
-});
+
+  console.log(
+    "Sum of priorities for task 1",
+    priorityArr.reduce((a, c) => a + c, 0)
+  );
+}
+
+// task2
+function task2(eachRucksackCompartments) {
+  priorityArr = [];
+
+  let groupArray = [];
+  let badge = [];
+  let badgeArr = [];
+  for (let i = 0; i < eachRucksackCompartments.length; i += 3) {
+    groupArray = eachRucksackCompartments.slice(i, i + 3);
+
+    const uniqueElementsArr = [];
+
+    groupArray.map((element) => {
+      const uniqueElementsSet = new Set(element);
+      uniqueElementsArr.push(uniqueElementsSet);
+    });
+
+    // console.log(uniqueElementsArr);
+
+    badge = [...uniqueElementsArr[1]].filter((x) =>
+      uniqueElementsArr[0].has(x)
+    );
+
+    const badgeSet = new Set(badge);
+
+    badge = [...uniqueElementsArr[2]].filter((x) => badgeSet.has(x));
+
+    badgeArr.push(badge);
+
+    if (/[a-z]/.test(badge[0])) {
+      priorityArr.push(badge[0].charCodeAt() - 96);
+    } else if (/[A-Z]/.test(badge[0])) {
+      priorityArr.push(badge[0].charCodeAt() - 38);
+    }
+  }
+  //   console.log("Badges Array", badgeArr);
+  console.log(
+    "Sum of priorities for task 2",
+    priorityArr.reduce((a, c) => a + c, 0)
+  );
+}
