bfe6d4594c026e5372a9818abc2db4d987f28fd2
liyasandeep <liyasandeep1@gmail.com>
2022-12-09 16:21:50 +0000

    task 2 solution

--- Day_6/index.js
+++ Day_6/index.js
@@ -2,19 +2,42 @@ const fs = require("fs");
 
 const signal = fs.readFileSync(__dirname + "/day6_Input.txt", "utf-8");
 
-let noOfCharactersProcessed = 0;
+task1(signal);
+task2(signal);
 
-for (let i = 0; i < signal.length; i++) {
-  const startOfPacketMarker = signal.substring(i, i + 4);
+function task1(signal) {
+  let noOfCharactersProcessed = 0;
 
-  const uniqueElements = new Set(startOfPacketMarker);
+  for (let i = 0; i < signal.length; i++) {
+    const startOfPacketMarker = signal.substring(i, i + 4);
 
-  if ([...uniqueElements].length === 4) {
-    noOfCharactersProcessed = i + 4;
-    console.log(
-      "Task 1:Number of characters processed before the first start-of-packet marker is detected: ",
-      noOfCharactersProcessed
-    );
-    break;
+    const uniqueElements = new Set(startOfPacketMarker);
+
+    if ([...uniqueElements].length === 4) {
+      noOfCharactersProcessed = i + 4;
+      console.log(
+        "Task 1:Number of characters processed before the first start-of-packet marker is : ",
+        noOfCharactersProcessed
+      );
+      break;
+    }
+  }
+}
+function task2(signal) {
+  let noOfCharactersProcessed = 0;
+
+  for (let i = 0; i < signal.length; i++) {
+    const startOfMessageMarker = signal.substring(i, i + 14);
+
+    const uniqueElements = new Set(startOfMessageMarker);
+
+    if ([...uniqueElements].length === 14) {
+      noOfCharactersProcessed = i + 14;
+      console.log(
+        "Task 1:Number of characters processed before the first start-of-message marker is : ",
+        noOfCharactersProcessed
+      );
+      break;
+    }
   }
 }
