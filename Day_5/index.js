d69011a694044258359cdb27621b4f69d4a8e5fe
liyasandeep <liyasandeep1@gmail.com>
2022-12-09 13:16:48 +0000

    task 2 solution

--- Day_5/index.js
+++ Day_5/index.js
@@ -43,7 +43,7 @@ fs.readFile(__dirname + "/day5_Input.txt", "utf-8")
     //   console.log(procedureArr);
 
     task1(stacksArrColoumWise, procedureArr);
-    // task2(stacksArrColoumWise, procedureArr);
+    task2(stacksArrColoumWise, procedureArr);
   });
 
 //   task 1
@@ -67,7 +67,27 @@ function task1(stacksArrColoumWise, procedureArr) {
 
   procedureArr.map((row) => moveCrate(row));
 
-  console.log("Resultant Stack:", stacksArrColoumWise);
+  console.log("Task1 Resultant Stack:", stacksArrColoumWise);
   const topOfStack = stacksArrColoumWise.map((el) => el.charAt(0)).join("");
-  console.log("Top of Stack:", topOfStack);
+  console.log("Task 1 Top of Stack:", topOfStack);
+}
+// Task 2
+function task2(stacksArrColoumWise, procedureArr) {
+  const moveCrate = (procedure) => {
+    procedure.map((el) => parseInt(el));
+
+    const [number, from, to] = procedure;
+
+    const movedCrate = stacksArrColoumWise[from - 1].substring(0, number);
+
+    stacksArrColoumWise = stacksArrColoumWise.map((el, i) =>
+      i === from - 1 ? el.slice(number) : i === to - 1 ? movedCrate + el : el
+    );
+  };
+
+  procedureArr.map((row) => moveCrate(row));
+
+  console.log(" Task 2 Resultant Stack:", stacksArrColoumWise);
+  const topOfStack = stacksArrColoumWise.map((el) => el.charAt(0)).join("");
+  console.log("Task 2 Top of Stack:", topOfStack);
 }
